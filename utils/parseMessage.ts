77b3bd8c60be8bb1e0f098e41c071bc11827f164
Taner Hacıoğlu <tanerfb1999@gmail.com>
2024-08-22 21:32:41 +0300

    added error message for incorrect formatted response

--- utils/parseMessage.ts
+++ utils/parseMessage.ts
@@ -1,16 +1,20 @@
-import type { SkillMessage } from "@/components/message";
+import type { ErrorMessage, SkillMessage } from "@/components/message";
 
 export function parseMessage(text: string) {
   const parts = text.split("\n");
 
-  const message: SkillMessage = {
+  if (parts.length < 5)
+    return {
+      type: "error",
+      text: "An error occurred while generating the response. Please try again.",
+    } as ErrorMessage;
+
+  return {
     type: "skill",
     skill: parts[0],
     attribute: parts[1],
     difficulty: parts[2],
     outcome: parts[3],
     text: parts[4],
-  };
-
-  return message;
+  } as SkillMessage;
 }
